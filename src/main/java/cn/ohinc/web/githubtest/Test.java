7811dc90e818ac233fb55287733cda0070f38567
Your Name <you@example.com>
2020-05-03 18:38:19 +0800

    db2

--- src/main/java/cn/ohinc/web/githubtest/Test.java
+++ src/main/java/cn/ohinc/web/githubtest/Test.java
@@ -1,12 +1,15 @@
 package cn.ohinc.web.githubtest;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Test {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String value;
 
