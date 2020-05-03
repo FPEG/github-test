1f659e73779adcf2e42d2dbb79b28a7263eb7aeb
Your Name <you@example.com>
2020-05-03 13:21:31 +0800

    asdtest

--- src/main/java/cn/ohinc/web/githubtest/TestController.java
+++ src/main/java/cn/ohinc/web/githubtest/TestController.java
@@ -11,10 +11,35 @@ import java.util.Date;
 @RestController
 public class TestController {
     @GetMapping("asd")
-    public String time() {
+    public Object time() {
+        class Temp {
+            public Temp(String time, String extra) {
+                this.time = time;
+                this.extra = extra;
+            }
+
+            private String time;
+            private String extra;
+
+            public String getTime() {
+                return time;
+            }
+
+            public void setTime(String time) {
+                this.time = time;
+            }
+
+            public String getExtra() {
+                return extra;
+            }
+
+            public void setExtra(String extra) {
+                this.extra = extra;
+            }
+        }
         Date now = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
-        return dateFormat.format(now);//格式化然后放入字符串中
+        return new Temp(dateFormat.format(now),"asdasdasd");
     }
 
 }
